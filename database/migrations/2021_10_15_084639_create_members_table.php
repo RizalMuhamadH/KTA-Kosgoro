e33e487996625059b2fe6d73dbe8e69a3aceffc7
Rizal muhamad H <rizalmuhamadh@gmail.com>
2021-10-15 16:33:26 +0700

    update migrate members

--- database/migrations/2021_10_15_084639_create_members_table.php
+++ database/migrations/2021_10_15_084639_create_members_table.php
@@ -17,8 +17,10 @@ class CreateMembersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('password');
             $table->string('phone')->unique();
             $table->string('nik')->unique();
+            $table->string('no_member')->unique();
             $table->string('photo');
             $table->string('address');
             $table->unsignedBigInteger('province_id');
@@ -26,11 +28,16 @@ class CreateMembersTable extends Migration
             $table->unsignedBigInteger('sub_district_id');
             $table->unsignedInteger('village_id');
             $table->string('post_code');
+            $table->string('qrcode');
+            $table->string('status');
+            $table->string('token');
+            $table->unsignedBigInteger('position_id');
             $table->timestamps();
 
             $table->foreign('province_id')->references('id')->on('provinces');
             $table->foreign('district_id')->references('id')->on('districts');
             $table->foreign('sub_district_id')->references('id')->on('sub_districts');
+            $table->foreign('position_id')->references('id')->on('positions');
             $table->foreign('village')->references('id')->on('villages');
         });
     }
