bd45a890aea03ac03b7f1ad914857c645d9f45fa
Rizal muhamad H <rizalmuhamadh@gmail.com>
2021-10-17 20:07:30 +0700

    elasticsearch repository

---
+++ app/Repository/elasticsearch.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repository;
+
+use Elasticsearch\ClientBuilder;
+
+class Elasticsearch {
+    protected $client;
+    public function __construct()
+    {
+        $this->client = ClientBuilder::create()->setHosts([
+            env("ELASTICSEARCH_HOSTS", "")
+        ])->build();
+    }
+
+    public function create($params)
+    {
+        return $this->client->create($params);
+    }
+
+    public function update($params)
+    {
+        return $this->client->update($params);
+    }
+
+    public function upsert($params)
+    {
+        return $this->client->upsert($params);
+    }
+
+    public function delete($params)
+    {
+        return $this->client->delete($params);
+    }
+
+    public function get($params)
+    {
+        return $this->client->get($params);
+    }
+}
