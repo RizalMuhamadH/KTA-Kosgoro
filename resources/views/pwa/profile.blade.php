98b660e20c1b45718e297b949893fc01b48e408f
yanuarfadhilah <yanuarfadhilah07@gmail.com>
2021-11-07 21:24:21 +0700

    Disable Print Member Card

--- resources/views/pwa/profile.blade.php
+++ resources/views/pwa/profile.blade.php
@@ -60,11 +60,11 @@
             </div>
         </div>
     </div>
-    <div class="center container">
+    {{-- <div class="center container">
         <a class="btn-small btn-register btn-large kosgoro-bg btn-print" href="{{route('pwa.download_kta',['id' => $data->id])}}">
             Cetak Kartu Anggota
         </a>
-    </div>
+    </div> --}}
 @endsection
 
 @push('page-javascript')
